3e7f12d8e8432d0a04047e2fbdca4e58137871c3
Carol Dawson <carol.dawson@ed.ac.uk>
2017-07-03 15:04:43 +0100

    Add Bowling Exercise

---
+++ exercises/bowling/src/main/java/BowlingGame.java
@@ -0,0 +1,11 @@
+public class BowlingGame {
+
+    public void Roll(int pins) {
+        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    }
+
+    public int Score() {
+        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    }
+
+}
